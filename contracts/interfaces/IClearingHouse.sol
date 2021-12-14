370e353c10a462180613a38e4d0ff8e90123ba6d
Kohshi Shiba <kohshi.shiba@gmail.com>
2021-12-14 14:35:37 +0900

    unused contracts deleted

--- contracts/interfaces/IClearingHouse.sol
+++
@@ -1,3 +0,0 @@
-pragma solidity 0.8.7;
-
-interface IClearingHouse {}
--- contracts/interfaces/IFeeModel.sol
+++
@@ -1,5 +0,0 @@
-pragma solidity 0.8.7;
-
-interface IFeeModel {
-    function getFee(uint256 _premium) external view returns (uint256);
-}
