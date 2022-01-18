3a151ea53fb6794bf57272a3e63f7b0ced2eb3be
blue32captain <blue32captain@gmail.com>
2022-01-18 19:38:14 +0800

    fix issue-#97

--- contracts/InsureDAOERC20.sol
+++ contracts/InsureDAOERC20.sol
@@ -102,7 +102,7 @@ contract InsureDAOERC20 is Context, IERC20, IERC20Metadata {
         override
         returns (bool)
     {
-        _transfer(_msgSender(), recipient, amount);
+        _transfer(msg.sender, recipient, amount);
         return true;
     }
 
@@ -132,7 +132,7 @@ contract InsureDAOERC20 is Context, IERC20, IERC20Metadata {
         override
         returns (bool)
     {
-        _approve(_msgSender(), spender, amount);
+        _approve(msg.sender, spender, amount);
         return true;
     }
 
@@ -156,13 +156,13 @@ contract InsureDAOERC20 is Context, IERC20, IERC20Metadata {
     ) external virtual override returns (bool) {
         _transfer(sender, recipient, amount);
 
-        uint256 currentAllowance = _allowances[sender][_msgSender()];
+        uint256 currentAllowance = _allowances[sender][msg.sender];
         require(
             currentAllowance >= amount,
             "ERC20: transfer amount exceeds allowance"
         );
 
-        _approve(sender, _msgSender(), currentAllowance - amount);
+        _approve(sender, msg.sender, currentAllowance - amount);
 
         return true;
     }
@@ -185,7 +185,7 @@ contract InsureDAOERC20 is Context, IERC20, IERC20Metadata {
         returns (bool)
     {
         _approve(
-            _msgSender(),
+            msg.sender,
             spender,
             _allowances[_msgSender()][spender] + addedValue
         );
@@ -217,7 +217,7 @@ contract InsureDAOERC20 is Context, IERC20, IERC20Metadata {
             "ERC20: decreased allowance below zero"
         );
 
-        _approve(_msgSender(), spender, currentAllowance - subtractedValue);
+        _approve(msg.sender, spender, currentAllowance - subtractedValue);
 
         return true;
     }
