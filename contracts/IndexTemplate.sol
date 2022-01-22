eec42a5e05b499f64c1abda622d03a67881e6bcb
otomarukanta <kanta208@gmail.com>
2022-01-22 10:18:15 +0900

    fix issue#300

--- contracts/IndexTemplate.sol
+++ contracts/IndexTemplate.sol
@@ -168,13 +168,12 @@ contract IndexTemplate is InsureDAOERC20, IIndexTemplate, IUniversalMarket {
         uint256 _totalLiquidity = totalLiquidity();
         vault.addValue(_amount, msg.sender, address(this));
 
-        if (_supply != 0 && _totalLiquidity != 0) {
-            _mintAmount = (_amount * _supply) / _totalLiquidity;
-        } else if (_supply != 0 && _totalLiquidity == 0) {
-            //when
+        if (_supply == 0) {
+            _mintAmount = _amount;
+        } else if (_totalLiquidity == 0) {
             _mintAmount = _amount * _supply;
         } else {
-            _mintAmount = _amount;
+            _mintAmount = (_amount * _supply) / _totalLiquidity;
         }
         emit Deposit(msg.sender, _amount, _mintAmount);
         //mint iToken
