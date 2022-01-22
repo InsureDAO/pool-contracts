ae8d700c18ddc1d362b431855dd0e8317b2ce67c
0xkenta <kenta0558kiichi614@gmail.com>
2022-01-22 20:31:12 +0900

    add test for setKeeper

--- test/unitary/Vault/unitVault.test.js
+++ test/unitary/Vault/unitVault.test.js
@@ -1267,4 +1267,18 @@ describe("Vault", function () {
 
     it("should allow controller to utilize", async () => {});
   });
+
+  describe("seKeeper", function () {
+    it("revert when the sender is not owner", async () => {
+      await expect(
+        vault
+          .connect(alice)
+          .setKeeper(alice.address)
+        ).to.revertedWith("Restricted: caller is not allowed to operate");
+    }) 
+
+    it("should succeed to set keeper", async () => {
+      
+    })
+  })
 });
