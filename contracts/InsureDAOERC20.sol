839b716910a4f82cd0c97a6204f470ae69c3bff0
0xkenta <kenta0558kiichi614@gmail.com>
2022-01-31 22:33:18 +0900

    excute _transfer outside of if sentence

--- contracts/InsureDAOERC20.sol
+++ contracts/InsureDAOERC20.sol
@@ -161,9 +161,9 @@ contract InsureDAOERC20 is Context, IERC20, IERC20Metadata {
                 );
                 _approve(sender, msg.sender, currentAllowance - amount);
             }
-
-            _transfer(sender, recipient, amount);
         }
+
+        _transfer(sender, recipient, amount);
 
         return true;
     }
