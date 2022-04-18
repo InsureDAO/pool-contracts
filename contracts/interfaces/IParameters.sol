6462bb992e7db1818f1e0315dd2caed068393aab
Shun Oikawa <oishun@Shuns-MacBook-Pro.local>
2022-04-19 01:04:15 +0900

    abstract to interface

--- contracts/interfaces/IParameters.sol
+++ contracts/interfaces/IParameters.sol
@@ -1,35 +1,31 @@
 pragma solidity 0.8.10;
 
-abstract contract IParameters {
-    function setVault(address _token, address _vault) external virtual;
+interface IParameters {
+    function setVault(address _token, address _vault) external;
 
-    function setLockup(address _address, uint256 _target) external virtual;
+    function setLockup(address _address, uint256 _target) external;
 
-    function setGrace(address _address, uint256 _target) external virtual;
+    function setGrace(address _address, uint256 _target) external;
 
-    function setMinDate(address _address, uint256 _target) external virtual;
+    function setMinDate(address _address, uint256 _target) external;
 
-    function setUpperSlack(address _address, uint256 _target) external virtual;
+    function setUpperSlack(address _address, uint256 _target) external;
 
-    function setLowerSlack(address _address, uint256 _target) external virtual;
+    function setLowerSlack(address _address, uint256 _target) external;
 
-    function setWithdrawable(address _address, uint256 _target)
-        external
-        virtual;
+    function setWithdrawable(address _address, uint256 _target) external;
 
-    function setPremiumModel(address _address, address _target)
-        external
-        virtual;
+    function setPremiumModel(address _address, address _target) external;
 
-    function setFeeRate(address _address, uint256 _target) external virtual;
+    function setFeeRate(address _address, uint256 _target) external;
 
-    function setMaxList(address _address, uint256 _target) external virtual;
+    function setMaxList(address _address, uint256 _target) external;
 
-    function setCondition(bytes32 _reference, bytes32 _target) external virtual;
+    function setCondition(bytes32 _reference, bytes32 _target) external;
 
-    function getOwner() external view virtual returns (address);
+    function getOwner() external view returns (address);
 
-    function getVault(address _token) external view virtual returns (address);
+    function getVault(address _token) external view returns (address);
 
     function getPremium(
         uint256 _amount,
@@ -37,51 +33,23 @@ abstract contract IParameters {
         uint256 _totalLiquidity,
         uint256 _lockedAmount,
         address _target
-    ) external view virtual returns (uint256);
+    ) external view returns (uint256);
 
-    function getFeeRate(address _target)
-        external
-        view
-        virtual
-        returns (uint256);
+    function getFeeRate(address _target) external view returns (uint256);
 
-    function getUpperSlack(address _target)
-        external
-        view
-        virtual
-        returns (uint256);
+    function getUpperSlack(address _target) external view returns (uint256);
 
-    function getLowerSlack(address _target)
-        external
-        view
-        virtual
-        returns (uint256);
+    function getLowerSlack(address _target) external view returns (uint256);
 
-    function getLockup(address _target) external view virtual returns (uint256);
+    function getLockup(address _target) external view returns (uint256);
 
-    function getWithdrawable(address _target)
-        external
-        view
-        virtual
-        returns (uint256);
+    function getWithdrawable(address _target) external view returns (uint256);
 
-    function getGrace(address _target) external view virtual returns (uint256);
+    function getGrace(address _target) external view returns (uint256);
 
-    function getMinDate(address _target)
-        external
-        view
-        virtual
-        returns (uint256);
+    function getMinDate(address _target) external view returns (uint256);
 
-    function getMaxList(address _target)
-        external
-        view
-        virtual
-        returns (uint256);
+    function getMaxList(address _target) external view returns (uint256);
 
-    function getCondition(bytes32 _reference)
-        external
-        view
-        virtual
-        returns (bytes32);
+    function getCondition(bytes32 _reference) external view returns (bytes32);
 }
