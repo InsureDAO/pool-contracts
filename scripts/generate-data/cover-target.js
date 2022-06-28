cd39a0e7d3d266d107b5aa01bb0a95059f79223b
Kohshi Shiba <kohshi.shiba@gmail.com>
2022-06-28 10:25:05 +0900

    update data generation script

--- scripts/generate-data/cover-target.js
+++ scripts/generate-data/cover-target.js
@@ -11,29 +11,37 @@ DON"T FORGET TO MODIFY DATA
 const list = [
   {
     id: "0x0000000000000000000000000000000000000000000000000000000000000001",
-    account: "0xa1e1822c5beab648c232b3e1f49959cfa80a22ec",
+    account: "0x0000000000000000000000000000000000000000",
+    loss: 100000000000000,
   },
   {
     id: "0x0000000000000000000000000000000000000000000000000000000000000001",
     account: "0x751ff30ed064ba16fce4f87b3557dea6f4decba0",
+    loss: 100000000000000,
   },
   {
     id: "0x0000000000000000000000000000000000000000000000000000000000000001",
     account: "0x7a14d3272bfd4742f365fe87272af227e02c4b3d",
+    loss: 100000000000000,
   },
   {
     id: "0x0000000000000000000000000000000000000000000000000000000000000001",
     account: "0x93ffa47b14215692141832f37eae16eb02bb59a3",
+    loss: 100000000000000,
   },
   {
     id: "0x0000000000000000000000000000000000000000000000000000000000000001",
     account: "0xc914FB29D729F37b8de6e5b7ef129B8D69458622",
+    loss: 100000000000000,
   },
 ];
 
 async function hashed(list) {
-  return list.map(({ id, account }) => {
-    return ethers.utils.solidityKeccak256(["bytes32", "address"], [id, account]);
+  return list.map(({ id, account, loss }) => {
+    return ethers.utils.solidityKeccak256(
+      ["bytes32", "address", "uint256"],
+      [id, account, loss]
+    );
   });
 }
 
