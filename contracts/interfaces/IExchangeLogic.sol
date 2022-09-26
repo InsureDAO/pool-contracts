7fca8a64656edb94b21cffbe541cbfec45dcfe31
maguro <maguro.devlife@gmail.com>
2022-09-26 15:55:08 +0900

    doc: natspec for ExchangeLogic

--- contracts/interfaces/IExchangeLogic.sol
+++ contracts/interfaces/IExchangeLogic.sol
@@ -7,6 +7,14 @@ pragma solidity ^0.8.12;
  * @notice Defines the basic interface for an InsureDAO Exchange Logic.
  **/
 interface IExchangeLogic {
+    /**
+     * @dev Returns swap function abi, which enables to perform interchangeability of various swap specs.
+     *      Caller exactly execute low level function call with abi.
+     * @param _tokenIn The token address to be swapped.
+     * @param _tokenOut The token address a caller receives.
+     * @param _amountIn The amount of token to be swapped.
+     * @param _amountOutMin The minimum amount the caller should receive.
+     */
     function abiEncodeSwap(
         address _tokenIn,
         address _tokenOut,
@@ -15,22 +23,37 @@ interface IExchangeLogic {
         address _to
     ) external view returns (bytes memory);
 
+    /**
+     * @dev Returns the contract address providing swap feature.
+     */
     function swapper() external returns (address);
 
+    /**
+     * @dev Returns the token amount to receive
+     */
     function estimateAmountOut(
         address _tokenIn,
         address _tokenOut,
         uint256 _amountIn
     ) external returns (uint256);
 
+    /**
+     * @dev Returns the token amount a caller need to provide for given amount.
+     */
     function estimateAmountIn(
         address _tokenIn,
         address _tokenOut,
         uint256 _amountMinOut
     ) external returns (uint256);
 
+    /**
+     * @dev Returns what portion of tokens to be lost from swap operation.
+     */
     function slippageTolerance() external view returns (uint256);
 
+    /**
+     * @dev Sets slippageTolerance of swap operations.
+     */
     function setSlippageTolerance(uint256 _tolerance) external;
 }
 
--- contracts/logics/ExchangeLogicUniswapV3.sol
+++ contracts/logics/ExchangeLogicUniswapV3.sol
@@ -11,24 +11,30 @@ import "../errors/CommonError.sol";
 /**
  * @title ExchangeLogicUniswapV3
  * @author @InsureDAO
- * @notice InsureDAO's Depeg insurance exchager of UniswapV3
+ * @notice InsureDAO's utilized asset exchager of UniswapV3
  **/
 contract ExchangeLogicUniswapV3 is IExchangeLogic {
+    /// @dev UniswapV3 router contract. See https://github.com/Uniswap/v3-periphery
     address public immutable swapper;
+    /// @dev UniswapV3 quoter contract. See https://github.com/Uniswap/v3-periphery
     IQuoter public immutable quoter;
 
+    /// @inheritdoc IExchangeLogic
     uint256 public slippageTolerance;
+
+    /// @dev What tier of swap fees this contract used. See detail for https://docs.uniswap.org/protocol/concepts/V3-overview/fees.
     uint24 public fee;
-    uint160 public sqrtPriceLimitX96;
+    /// @dev The limit for the price the swap will push the pool to. We disable this feature. See detail for https://docs.uniswap.org/protocol/guides/swaps/single-swaps
+    uint160 public constant sqrtPriceLimitX96 = 0;
 
     constructor(address _router, address _quoter) {
         swapper = _router;
         quoter = IQuoter(_quoter);
         slippageTolerance = 985_000; // 1.5%
         fee = 3_000;
-        sqrtPriceLimitX96 = 0;
     }
 
+    /// @inheritdoc IExchangeLogic
     function abiEncodeSwap(
         address _tokenIn,
         address _tokenOut,
@@ -59,6 +65,7 @@ contract ExchangeLogicUniswapV3 is IExchangeLogic {
         return abi.encodeWithSelector(ISwapRouter.exactInputSingle.selector, params);
     }
 
+    /// @inheritdoc IExchangeLogic
     function estimateAmountOut(
         address _tokenIn,
         address _tokenOut,
@@ -69,6 +76,7 @@ contract ExchangeLogicUniswapV3 is IExchangeLogic {
         return quoter.quoteExactInputSingle(_tokenIn, _tokenOut, fee, _amountIn, sqrtPriceLimitX96);
     }
 
+    /// @inheritdoc IExchangeLogic
     function estimateAmountIn(
         address _tokenIn,
         address _tokenOut,
@@ -79,6 +87,7 @@ contract ExchangeLogicUniswapV3 is IExchangeLogic {
         return quoter.quoteExactOutputSingle(_tokenIn, _tokenOut, fee, _amountOutMin, sqrtPriceLimitX96);
     }
 
+    /// @inheritdoc IExchangeLogic
     function setSlippageTolerance(uint256 _tolerance) external {
         if (_tolerance == 0) revert ZeroSlippageTolerance();
         if (_tolerance > 1e6) revert SlippageToleranceOutOfRange();
