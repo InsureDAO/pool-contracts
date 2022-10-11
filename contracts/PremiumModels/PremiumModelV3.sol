28e61090c86d11fb0fb65fa7cd5138a2d8341a9b
Shun Oikawa <shunoikawa1112@gmail.com>
2022-10-12 01:45:21 +0900

    setRate for each market

--- contracts/PremiumModels/PremiumModelV3.sol
+++ contracts/PremiumModels/PremiumModelV3.sol
@@ -7,22 +7,24 @@ pragma solidity 0.8.12;
  * SPDX-License-Identifier: GPL-3.0
  */
 
-import "../interfaces/IPremiumModelV2.sol";
+import "../interfaces/IPremiumModelV3.sol";
 import "../interfaces/IOwnership.sol";
 import "hardhat/console.sol";
 
-contract PremiumModelV3 is IPremiumModelV2 {
+contract PremiumModelV3 is IPremiumModelV3 {
     IOwnership public immutable ownership;
 
-    mapping(address => uint256) public baseRates;
+    struct Rate {
+        uint64 baseRate;
+        uint64 rateSlope1;
+        uint64 rateSlope2;
+        uint64 optimalUtilizeRatio;
+    }
 
-    uint256 public immutable rateSlope1;
-    uint256 public immutable rateSlope2;
-    uint256 public immutable OPTIMAL_UTILIZE_RATIO;
+    mapping(address => Rate) public rates;
 
-    uint256 internal immutable MAX_EXCESS_UTILIZE_RATIO;
-    uint256 internal constant MAX_RATE = 1e6;
-    uint256 internal constant MAGIC_SCALE = 1e6; //100%
+    uint256 internal constant MAX_RATIO = 1e6; //100%
+    uint256 internal constant MAGIC_SCALE = 1e6;
 
     modifier onlyOwner() {
         require(ownership.owner() == msg.sender, "Caller is not allowed to operate");
@@ -31,25 +33,20 @@ contract PremiumModelV3 is IPremiumModelV2 {
 
     constructor(
         address _ownership,
-        uint256 _defaultRate,
-        uint256 _rateSlope1,
-        uint256 _rateSlope2,
-        uint256 _OPTIMAL_USAGE_RATIO
+        uint64 _defaultRate,
+        uint64 _defaultRateSlope1,
+        uint64 _defaultRateSlope2,
+        uint64 _optimalUtilizeRatio
     ) {
         require(_ownership != address(0), "zero address");
         require(_defaultRate != 0, "rate is zero");
-        require(_rateSlope1 != 0, "slope1 is zero");
-        require(_rateSlope2 != 0, "slope2 is zero");
-        require(_OPTIMAL_USAGE_RATIO != 0, "ratio is zero");
-
-        require(_OPTIMAL_USAGE_RATIO <= MAX_RATE, "exceed max rate");
+        require(_defaultRateSlope1 != 0, "slope1 is zero");
+        require(_defaultRateSlope2 != 0, "slope2 is zero");
+        require(_optimalUtilizeRatio != 0, "ratio is zero");
+        require(_optimalUtilizeRatio <= MAX_RATIO, "exceed max rate");
 
         ownership = IOwnership(_ownership);
-        baseRates[address(0)] = _defaultRate;
-        rateSlope1 = _rateSlope1;
-        rateSlope2 = _rateSlope2;
-        OPTIMAL_UTILIZE_RATIO = _OPTIMAL_USAGE_RATIO;
-        MAX_EXCESS_UTILIZE_RATIO = MAGIC_SCALE - _OPTIMAL_USAGE_RATIO;
+        rates[address(0)] = Rate(_defaultRate, _defaultRateSlope1, _defaultRateSlope2, _optimalUtilizeRatio);
     }
 
     function getCurrentPremiumRate(
@@ -62,7 +59,9 @@ contract PremiumModelV3 is IPremiumModelV2 {
         if (_lockedAmount != 0 && _totalLiquidity != 0) {
             _utilizedRate = (_lockedAmount * MAGIC_SCALE) / _totalLiquidity;
         }
-        return _getPremiumRate(_market, _utilizedRate);
+
+        Rate memory _rate = _getRate(_market);
+        return _getPremiumRate(_rate, _utilizedRate);
     }
 
     function getPremiumRate(
@@ -90,17 +89,29 @@ contract PremiumModelV3 is IPremiumModelV2 {
         require(_amount + _lockedAmount <= _totalLiquidity, "Amount exceeds total liquidity");
         require(_totalLiquidity != 0, "totalLiquidity is 0");
 
+        Rate memory _rate = _getRate(_market);
+
         uint256 _utilizedRateBefore = (_lockedAmount * MAGIC_SCALE) / _totalLiquidity;
         uint256 _utilizedRateAfter = ((_lockedAmount + _amount) * MAGIC_SCALE) / _totalLiquidity;
         uint256 _premium;
 
-        if (_utilizedRateAfter <= OPTIMAL_UTILIZE_RATIO || OPTIMAL_UTILIZE_RATIO <= _utilizedRateBefore) {
+        if (_utilizedRateAfter <= _rate.optimalUtilizeRatio || _rate.optimalUtilizeRatio <= _utilizedRateBefore) {
             //slope1 or 2
-            _premium += _calcOneSidePremiumAmount(_market, _totalLiquidity, _utilizedRateBefore, _utilizedRateAfter);
+            _premium += _calcOneSidePremiumAmount(_rate, _totalLiquidity, _utilizedRateBefore, _utilizedRateAfter);
         } else {
             //slope1 & 2
-            _premium += _calcOneSidePremiumAmount(_market, _totalLiquidity, _utilizedRateBefore, OPTIMAL_UTILIZE_RATIO);
-            _premium += _calcOneSidePremiumAmount(_market, _totalLiquidity, OPTIMAL_UTILIZE_RATIO, _utilizedRateAfter);
+            _premium += _calcOneSidePremiumAmount(
+                _rate,
+                _totalLiquidity,
+                _utilizedRateBefore,
+                _rate.optimalUtilizeRatio
+            );
+            _premium += _calcOneSidePremiumAmount(
+                _rate,
+                _totalLiquidity,
+                _rate.optimalUtilizeRatio,
+                _utilizedRateAfter
+            );
         }
 
         _premium = (_premium * _term) / 365 days;
@@ -108,23 +119,29 @@ contract PremiumModelV3 is IPremiumModelV2 {
         return _premium;
     }
 
-    function setRate(address _market, uint256 _rate) external onlyOwner {
-        baseRates[_market] = _rate;
+    function getRate(address _market) external view returns (Rate memory) {
+        return _getRate(_market);
+    }
+
+    function setRate(address _market, Rate calldata _rate) external onlyOwner {
+        require(_rate.optimalUtilizeRatio <= MAX_RATIO, "exceed max rate");
+        rates[_market] = _rate;
     }
 
     function _calcOneSidePremiumAmount(
-        address _market,
+        Rate memory _rate,
         uint256 _totalLiquidity,
         uint256 _utilizedRateBefore,
         uint256 _utilizedRateAfter
-    ) internal view returns (uint256) {
+    ) internal pure returns (uint256) {
         require(
-            !(_utilizedRateBefore < OPTIMAL_UTILIZE_RATIO && OPTIMAL_UTILIZE_RATIO < _utilizedRateAfter),
+            !(_utilizedRateBefore < _rate.optimalUtilizeRatio && _utilizedRateAfter > _rate.optimalUtilizeRatio),
             "Contains the corner"
         );
 
-        uint256 _currentPremiumBefore = _getPremiumRate(_market, _utilizedRateBefore);
-        uint256 _currentPremiumAfter = _getPremiumRate(_market, _utilizedRateAfter);
+        uint256 _currentPremiumBefore = _getPremiumRate(_rate, _utilizedRateBefore);
+        uint256 _currentPremiumAfter = _getPremiumRate(_rate, _utilizedRateAfter);
+
         uint256 _avePremiumRate = (_currentPremiumBefore + _currentPremiumAfter) / 2;
         uint256 _amount = ((_utilizedRateAfter - _utilizedRateBefore) * _totalLiquidity) / MAGIC_SCALE;
 
@@ -136,28 +153,35 @@ contract PremiumModelV3 is IPremiumModelV2 {
     /**
      * @dev return BaseRate when _utilizedRate is 0;
      */
-    function _getPremiumRate(address _market, uint256 _utilizedRate) internal view returns (uint256) {
-        uint256 _currentPremiumRate = _getBaseRate(_market);
+    function _getPremiumRate(Rate memory _rate, uint256 _utilizedRate) internal pure returns (uint256) {
+        uint256 _currentPremiumRate = _rate.baseRate;
+        uint256 _maxExcessUtilizeRatio = MAGIC_SCALE - _rate.optimalUtilizeRatio;
 
-        if (_utilizedRate > OPTIMAL_UTILIZE_RATIO) {
+        if (_utilizedRate > _rate.optimalUtilizeRatio) {
             uint256 excessUtilizeRatio;
             unchecked {
-                excessUtilizeRatio = _utilizedRate - OPTIMAL_UTILIZE_RATIO;
+                excessUtilizeRatio = _utilizedRate - _rate.optimalUtilizeRatio;
             }
-            _currentPremiumRate += (rateSlope1 + ((rateSlope2 * excessUtilizeRatio) / MAX_EXCESS_UTILIZE_RATIO));
+            _currentPremiumRate += (_rate.rateSlope1 +
+                ((_rate.rateSlope2 * excessUtilizeRatio) / _maxExcessUtilizeRatio));
         } else {
-            _currentPremiumRate += (rateSlope1 * _utilizedRate) / OPTIMAL_UTILIZE_RATIO;
+            _currentPremiumRate += (_rate.rateSlope1 * _utilizedRate) / _rate.optimalUtilizeRatio;
         }
 
         return _currentPremiumRate;
     }
 
-    function _getBaseRate(address _market) internal view returns (uint256) {
-        uint256 _rate = baseRates[_market];
-        if (_rate == 0) {
-            return baseRates[address(0)];
-        } else {
-            return _rate;
-        }
+    function _getRate(address _market) internal view returns (Rate memory) {
+        Rate memory _rate = rates[_market];
+        Rate memory _defaultRate = rates[address(0)];
+
+        uint64 _baseRate = _rate.baseRate == 0 ? _defaultRate.baseRate : _rate.baseRate;
+        uint64 _rateSlope1 = _rate.rateSlope1 == 0 ? _defaultRate.rateSlope1 : _rate.rateSlope1;
+        uint64 _rateSlope2 = _rate.rateSlope2 == 0 ? _defaultRate.rateSlope2 : _rate.rateSlope2;
+        uint64 _optimalUtilizeRatio = _rate.optimalUtilizeRatio == 0
+            ? _defaultRate.optimalUtilizeRatio
+            : _rate.optimalUtilizeRatio;
+
+        return Rate(_baseRate, _rateSlope1, _rateSlope2, _optimalUtilizeRatio);
     }
 }
